c7d9b76bbcdb48a21dc1f3bf4e89a6bc34c6bb9c
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-23 09:35:23 +0300

    node

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const names = require("./NodeTutorial/3-names");
 const sayHi = require("./NodeTutorial/4-utils");
 
 const data = require("./NodeTutorial/6-altExports");
-console.log(data);
+
+require("./7-test");
 
 /* sayHi(names.labs);
 sayHi(names.admin); */
